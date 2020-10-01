16eff3aa174ba1298f91f3fa17805418e2f4acf7
Felipe <f.guimaraes@SOLUTION-FGUIMARAES.local>
2020-10-01 11:16:56 -0400

    date and hour
    Signed-off-by: Felipe <f.guimaraes@SOLUTION-FGUIMARAES.local>

--- components/Point.js
+++ components/Point.js
@@ -1,25 +1,78 @@
 import React, {useState, useEffect} from 'react';
-import {View, Text, StyleSheet} from 'react-native';
+import {View, Text, StyleSheet, TouchableOpacity} from 'react-native';
 
 export default function Point(){
+    const [date, setDate] = useState(null);
+    const [hour ,setHour] = useState(null);
+    const [minute, setMinute] = useState(null);
+    const [textHour, setTextHour] = useState(null);
+
+    useEffect(() => {
+        var day = null;
+        var month = null;
+        var year = new Date().getFullYear().toString();
+        var hour = null;
+        var minute = null;
+        var seconds = null;
+
+        {new Date().getDate() < 10 ? 
+        day = "0" + (new Date().getDay().toString())
+        :
+        day = new Date().getDay().toString()
+        }
+        {new Date().getMonth() < 10 ?
+        month = "0" + (new Date().getMonth().toString())
+        :
+        month = new Date().getMonth().toString()        
+        }
+
+        const timer = setInterval(()=>{
+            {new Date().getHours() < 10 ?
+            hour = "0"+ (new Date().getHours().toString())
+            
+            :
+            hour = new Date().getHours().toString()
+            }
+            {new Date().getMinutes() < 10 ?
+            minute = "0"+ (new Date().getMinutes().toString())
+            
+            :
+            minute = new Date().getMinutes().toString()
+            }
+            {new Date().getSeconds() < 10 ?
+            seconds = "0"+ (new Date().getSeconds().toString())
+            :
+            seconds = new Date().getSeconds().toString()
+            }
+    
+            setTextHour(hour+":"+minute+":"+seconds)
+        },100)
+        
+        
+        setDate(day+"/"+month+"/"+year)
+
+        return function cleanup(){
+            clearInterval(timer);
+        }
+    },)
     return(
         <View style={styles.container}>
-            <View>
-                <Text>Dia</Text>
+            <View style={styles.dateContainer}>
+                <Text style={styles.fontDate}>{date}</Text>
             </View>
             <View style={styles.containerPoint}>
                 <View style={styles.containerLabel}>
                     <View style={styles.labels}>
-                        <Text>Entrada</Text>
+                        <Text>Entrada:</Text>
                     </View>
                     <View style={styles.labels}>
-                        <Text>Saida do almoço</Text>
+                        <Text>Saida do almoço:</Text>
                     </View>
                     <View style={styles.labels}>
-                        <Text>Volta do almoço</Text>
+                        <Text>Volta do almoço:</Text>
                     </View>
                     <View style={styles.labels}>
-                        <Text>Saida</Text>
+                        <Text>Saida:</Text>
                     </View>
                 </View>
                 <View style={styles.containerHours}>
@@ -37,11 +90,13 @@ export default function Point(){
                     </View>
                 </View>
             </View>
-            <View>
-                <Text>Hora</Text>
+            <View style={styles.hourContainer}>
+                <Text style={styles.hourFont}>{textHour}</Text>
             </View>
-            <View>
-                <Text>Bater ponto</Text>
+            <View style={styles.containerButton}>
+                <TouchableOpacity style={styles.pointButton}>
+                    <Text style={styles.fontButton}>Bater ponto</Text>
+                </TouchableOpacity>
             </View>
         </View>
     )
@@ -62,7 +117,7 @@ const styles = StyleSheet.create({
         justifyContent:"space-between",
         alignItems:"flex-start",
         width:"50%",
-        backgroundColor:"#3c3c3c"
+        backgroundColor:"#00bcd4"
     },
     containerHours:{
         flexDirection:"column",
@@ -73,5 +128,45 @@ const styles = StyleSheet.create({
     },
     labels: {
         margin:10
+    },
+    dateContainer:{
+        width:"100%",
+        alignItems:"center",
+        justifyContent:"center",
+        marginVertical:20
+    },
+    fontDate: {
+        fontSize:40,
+        fontWeight:"bold"
+    },
+    containerButton:{
+        width:"100%",
+        justifyContent:"center",
+        alignItems:"center",
+        marginVertical:20,
+        height:200
+    },
+    pointButton:{
+        backgroundColor:"#008394",
+        height:50,
+        width:"70%",
+        borderRadius:10,
+        alignItems:"center",
+        justifyContent:"center"
+    },
+    fontButton:{
+        color:"#fff",
+        fontWeight:"bold",
+        fontSize:17
+    },
+    hourContainer: {
+        width:"100%",
+        height:200,
+        justifyContent:"center",
+        alignItems:"center"
+    },
+    hourFont:{
+        fontSize:60,
+        fontWeight:"bold"
     }
 })
